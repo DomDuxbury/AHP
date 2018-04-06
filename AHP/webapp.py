952e6941ed42a4855594b0b3972189b0b106e994
Domduxbury <domduxbury@hotmail.com>
2018-04-06 10:22:24 +0100

    Added cors support

--- AHP/webapp.py
+++ AHP/webapp.py
@@ -1,10 +1,12 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
 from main import calc_all_weights
 import config
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 def validate_request(request):
